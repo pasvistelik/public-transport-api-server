63816d03c836727eaa621f0db2abd996bd93256d
pasvistelik <pasvistelik@gmail.com>
2017-03-31 13:10:54 +0300

    Put a IgnoringFragments class in a separate module.

--- routes/optimalRoute.js
+++ routes/optimalRoute.js
@@ -5,7 +5,7 @@ var Points = require('../modules/points');
 var distance = require('../modules/geoCoords').distance;
 
 
-
+var IgnoringFragments = require('../modules/ignoringFragments');
 
 
 
@@ -14,28 +14,6 @@ var distance = require('../modules/geoCoords').distance;
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
-
-
-class IgnoringFragments extends Array {
-    constructor() {
-        super();
-        if (arguments[0] != undefined && arguments[0] != null && arguments[0].length != 0) {
-            try {
-                for (var i = 0, n = arguments[0].length, item = arguments[0][0]; i < n; item = arguments[0][++i])
-                this.push(item);
-            }
-            catch (ex) {
-                console.log(ex.message);
-            }
-        }
-    }
-    contains(stationCode, routeCode, fromStationCode) {
-        for (var i = 0, n = this.length, r = this[0]; i < n; r = this[++i]) {
-            if (r.routeCode == routeCode && r.stationCode == stationCode && r.fromStationCode == fromStationCode) return true;
-        }
-        return false;
-    }
-}
 
 class OptimalRoutesCollection extends Array {
     constructor(nowPos, needPos, time, types, speed, dopTimeMinutes) {
