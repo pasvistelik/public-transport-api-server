9c61954f92bcfb297bbd732729919cd8e9caa1ae
pasvistelik <pasvistelik@gmail.com>
2017-03-31 13:23:45 +0300

    Put all classes in a separate modules.

---
+++ modules/optimalRoutesCollection.js
@@ -0,0 +1,69 @@
+﻿var IgnoringFragments = require('./ignoringFragments');
+var OptimalRoute = require('./optimalRoute');
+var OptimalWay = require('./optimalWay');
+
+class OptimalRoutesCollection extends Array {
+    constructor(nowPos, needPos, time, types, speed, dopTimeMinutes) {
+        super();
+
+        this.push(new OptimalRoute(nowPos, needPos, time, types, speed, dopTimeMinutes));
+
+        var ignoringRoutes = new Array();
+
+        var ignoringFragments = new IgnoringFragments();
+
+        for (var selectedOptimalRoute = this[0]; selectedOptimalRoute != null; selectedOptimalRoute.setVisited(), selectedOptimalRoute = this.selectOptimalRouteWithMinimalMark()) {
+            var ddd = 0.25;
+
+            ignoringRoutes = new Array();
+            // Проходим по всем ребрам выбранного пути и строим новые маршруты при удалении ребер:
+            for (var tmpP = selectedOptimalRoute.myPoints.finalPoint; tmpP.previousPoint != null; tmpP = tmpP.previousPoint) {
+                if (tmpP.fromWhichRoute != null && !ignoringRoutes.includes(tmpP.fromWhichRoute)) ignoringRoutes.push(tmpP.fromWhichRoute);
+            }
+            for (var i = 0, n = ignoringRoutes.length, r = ignoringRoutes[0]; i < n; r = ignoringRoutes[++i]) {
+                if (selectedOptimalRoute.ignoringRoutes.includes(r)) continue;
+                var ignoringRoutesAdd = new Array();
+                ignoringRoutesAdd = ignoringRoutesAdd.concat(selectedOptimalRoute.ignoringRoutes);
+                ignoringRoutesAdd.push(r);
+                var tmpOptimalRoute = new OptimalRoute(nowPos, needPos, time, types, speed, dopTimeMinutes, ignoringRoutesAdd);
+
+                if (tmpOptimalRoute.totalTimeSeconds <= this[0].totalTimeSeconds / ddd) {
+                    var tmpJSON = JSON.stringify(tmpOptimalRoute.points);
+                    var ok = false;
+                    for (var j = 0, m = this.length, opt = this[0]; j < m; opt = this[++j]) {
+                        if (JSON.stringify(opt.points) == tmpJSON) {
+                            ok = true;
+                            break;
+                        }
+                    }
+                    if (ok) continue;
+                    this.push(tmpOptimalRoute);
+                }
+            }
+        }
+    }
+    getOptimalWays() {
+        var result = new Array();
+        for (var i = 0, n = this.length, r = this[0]; i < n; r = this[++i]) {
+            result.push(new OptimalWay(r));
+        }
+        return result;
+    }
+    selectOptimalRouteWithMinimalMark() {
+        var p = null;
+        for (var i = 0, n = this.length, t = this[0]; i < n; t = this[++i]) {
+            if (!(t.isVisited)) {
+                p = t;
+                for (t = this[++i]; i < n; t = this[++i]) {
+                    if (!(t.isVisited) && t.totalTimeSeconds < p.totalTimeSeconds) {
+                        p = t;
+                    }
+                }
+                return p;
+            }
+        }
+        return null;
+    }
+}
+
+module.exports = OptimalRoutesCollection;
