25c293328dca2bb28707ea4a7e0907340b1f2109
pasvistelik <pasvistelik@gmail.com>
2017-07-27 15:03:12 +0300

    Put main app code with async functions in Promise.

--- app.js
+++ app.js
@@ -6,55 +6,59 @@ import cookieParser from 'cookie-parser';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 
-
-import DataProvider from 'public-transport-server-code/lib/dataProvider';
-DataProvider.loadDataAndInitialize();
-
 import my_stations from './routes/stations';
 import my_routes from './routes/routes';
 import my_timetables from './routes/timetables';
 import optimalRoute from './routes/optimalRoute';
 
+import DataProvider from 'public-transport-server-code/lib/dataProvider';
+
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+(new Promise(async function(resolve, reject) {
+    await DataProvider.loadDataAndInitialize();
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(cors());
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+    // view engine setup
+    app.set('views', path.join(__dirname, 'views'));
+    app.set('view engine', 'pug');
 
-
-app.use('/stations', my_stations);
-app.use('/routes', my_routes);
-app.use('/timetables', my_timetables);
-
-app.use('/optimalRoute', optimalRoute);
+    // uncomment after placing your favicon in /public
+    //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+    app.use(cors());
+    app.use(logger('dev'));
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({ extended: false }));
+    app.use(cookieParser());
+    app.use(express.static(path.join(__dirname, 'public')));
 
 
+    app.use('/stations', my_stations);
+    app.use('/routes', my_routes);
+    app.use('/timetables', my_timetables);
 
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-});
+    app.use('/optimalRoute', optimalRoute);
 
-// error handler
-app.use(function (err, req, res, next) {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-    // render the error page
-    res.status(err.status || 500);
-    res.render('error');
-});
+
+    // catch 404 and forward to error handler
+    app.use(function (req, res, next) {
+        var err = new Error('Not Found');
+        err.status = 404;
+        next(err);
+    });
+
+    // error handler
+    app.use(function (err, req, res, next) {
+        // set locals, only providing error in development
+        res.locals.message = err.message;
+        res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+        // render the error page
+        res.status(err.status || 500);
+        res.render('error');
+    });
+
+
+}));
 
 module.exports = app;
--- build/app.js
+++ build/app.js
@@ -1,5 +1,9 @@
 'use strict';
 
+var _regenerator = require('babel-runtime/regenerator');
+
+var _regenerator2 = _interopRequireDefault(_regenerator);
+
 var _express = require('express');
 
 var _express2 = _interopRequireDefault(_express);
@@ -28,10 +32,6 @@ var _cors = require('cors');
 
 var _cors2 = _interopRequireDefault(_cors);
 
-var _dataProvider = require('public-transport-server-code/lib/dataProvider');
-
-var _dataProvider2 = _interopRequireDefault(_dataProvider);
-
 var _stations = require('./routes/stations');
 
 var _stations2 = _interopRequireDefault(_stations);
@@ -48,47 +48,67 @@ var _optimalRoute = require('./routes/optimalRoute');
 
 var _optimalRoute2 = _interopRequireDefault(_optimalRoute);
 
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+var _dataProvider = require('public-transport-server-code/lib/dataProvider');
 
-_dataProvider2.default.loadDataAndInitialize();
+var _dataProvider2 = _interopRequireDefault(_dataProvider);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 var app = (0, _express2.default)();
 
-// view engine setup
-app.set('views', _path2.default.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+new Promise(function _callee(resolve, reject) {
+    return _regenerator2.default.async(function _callee$(_context) {
+        while (1) {
+            switch (_context.prev = _context.next) {
+                case 0:
+                    _context.next = 2;
+                    return _regenerator2.default.awrap(_dataProvider2.default.loadDataAndInitialize());
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use((0, _cors2.default)());
-app.use((0, _morgan2.default)('dev'));
-app.use(_bodyParser2.default.json());
-app.use(_bodyParser2.default.urlencoded({ extended: false }));
-app.use((0, _cookieParser2.default)());
-app.use(_express2.default.static(_path2.default.join(__dirname, 'public')));
+                case 2:
 
-app.use('/stations', _stations2.default);
-app.use('/routes', _routes2.default);
-app.use('/timetables', _timetables2.default);
+                    // view engine setup
+                    app.set('views', _path2.default.join(__dirname, 'views'));
+                    app.set('view engine', 'pug');
 
-app.use('/optimalRoute', _optimalRoute2.default);
+                    // uncomment after placing your favicon in /public
+                    //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+                    app.use((0, _cors2.default)());
+                    app.use((0, _morgan2.default)('dev'));
+                    app.use(_bodyParser2.default.json());
+                    app.use(_bodyParser2.default.urlencoded({ extended: false }));
+                    app.use((0, _cookieParser2.default)());
+                    app.use(_express2.default.static(_path2.default.join(__dirname, 'public')));
 
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-});
+                    app.use('/stations', _stations2.default);
+                    app.use('/routes', _routes2.default);
+                    app.use('/timetables', _timetables2.default);
 
-// error handler
-app.use(function (err, req, res, next) {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
+                    app.use('/optimalRoute', _optimalRoute2.default);
 
-    // render the error page
-    res.status(err.status || 500);
-    res.render('error');
+                    // catch 404 and forward to error handler
+                    app.use(function (req, res, next) {
+                        var err = new Error('Not Found');
+                        err.status = 404;
+                        next(err);
+                    });
+
+                    // error handler
+                    app.use(function (err, req, res, next) {
+                        // set locals, only providing error in development
+                        res.locals.message = err.message;
+                        res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+                        // render the error page
+                        res.status(err.status || 500);
+                        res.render('error');
+                    });
+
+                case 16:
+                case 'end':
+                    return _context.stop();
+            }
+        }
+    }, null, this);
 });
 
 module.exports = app;
