24162ebf477ad4731e4351cde9ef6a9b81edee4c
pasvistelik <pasvistelik@gmail.com>
2018-02-01 19:13:34 +0300

    upd

--- build/config.js
+++ build/config.js
@@ -4,9 +4,13 @@ Object.defineProperty(exports, "__esModule", {
     value: true
 });
 exports.default = {
-    apiGetStationsUrl: "https://optimalway.github.io/json/stations.json?v=15012018", //"stations"
-    apiGetRoutesUrl: "https://optimalway.github.io/json/routes.json?v=15012018", //"routes"
-    apiGetTimetablesUrl: "https://optimalway.github.io/json/timetables.json?v=15012018", //"timetables"
+    apiGetStationsUrl: "https://optimalway.github.io/json/stations_minsk_grodno.json?v=01022018", //"stations" minsk + grodno
+    apiGetRoutesUrl: "https://optimalway.github.io/json/routes_minsk_grodno.json?v=01022018", //"routes" minsk + grodno
+    apiGetTimetablesUrl: "https://optimalway.github.io/json/timetables_minsk_grodno.json?v=01022018", //"timetables" minsk + grodno
+
+    //apiGetStationsUrl: "https://optimalway.github.io/json/stations.json?v=15012018", //"stations"
+    //apiGetRoutesUrl: "https://optimalway.github.io/json/routes.json?v=15012018", //"routes"
+    //apiGetTimetablesUrl: "https://optimalway.github.io/json/timetables.json?v=15012018", //"timetables"
 
     databaseEnabled: false,
     databaseHost: 'localhost',
